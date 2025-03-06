236116dd4910d06750eaa29384323d0a2501eb90
Vasyl <priymak17ukrvaha@gmail.com>
2025-03-07 00:08:11 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1.then(() => {
+  const successMessage = document.createElement('div');
+
+  successMessage.classList.add('message');
+  successMessage.textContent = 'Promise was resolved!';
+  document.body.append(successMessage);
+});
+
+promise2
+  .then(() => {
+    const successMessage = document.createElement('div');
+
+    successMessage.classList.add('message');
+    successMessage.textContent = 'Promise was resolved!';
+    document.body.append(successMessage);
+  })
+  .catch((error) => {
+    const errorMessage = document.createElement('div');
+
+    errorMessage.classList.add('message', 'error-message');
+    errorMessage.textContent = error.message;
+    document.body.append(errorMessage);
+  });
